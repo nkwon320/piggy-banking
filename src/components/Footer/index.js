44a06b8942282f6a2fcaaff38b0a9d35cfb15c0a
Noah <nkwon.work@gmail.com>
2021-02-03 14:06:06 -0800

    finished React project with useState, Smooth Scroll, Styled Components, BrowserRouter, Switch, and Route

---
+++ src/components/Footer/index.js
@@ -0,0 +1,94 @@
+import React from 'react'
+import { animateScroll as scroll} from 'react-scroll';
+
+import {
+    FooterContainer,
+    FooterWrap,
+    FooterLinksContainer,
+    FooterLinksWrapper,
+    FooterLinkItems,
+    FooterLinkTitle,
+    FooterLink, 
+    SocialMedia,
+    SocialMediaWrap, 
+    SocialLogo,
+    WebsiteRights,
+    SocialIcons,
+    SocialIconLink
+} from './FooterElements';
+
+import {FaFacebook, FaInstagram, FaYoutube, FaTwitter, FaLinkedin} from 'react-icons/fa';
+
+const Footer = () => {
+    const toggleHome = () => {
+        scroll.scrollToTop();
+    }
+
+    return (
+        <FooterContainer>
+            <FooterWrap>
+                <FooterLinksContainer>
+                    <FooterLinksWrapper>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About Us</FooterLinkTitle>
+                            <FooterLink to="/">How it Works</FooterLink>
+                            <FooterLink to="/">Testimonials</FooterLink>
+                            <FooterLink to="/">Careers</FooterLink>
+                            <FooterLink to="/">Investors</FooterLink>
+                            <FooterLink to="/">Terms of Service</FooterLink>
+                        </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>Contact Us</FooterLinkTitle>
+                            <FooterLink to="/">Contact</FooterLink>
+                            <FooterLink to="/">Support</FooterLink>
+                            <FooterLink to="/">Destinations</FooterLink>
+                            <FooterLink to="/">Sponsorships</FooterLink>
+                        </FooterLinkItems>
+                    </FooterLinksWrapper>
+                    <FooterLinksWrapper>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>Videos</FooterLinkTitle>
+                            <FooterLink to="/">Submit Video</FooterLink>
+                            <FooterLink to="/">Ambassadors</FooterLink>
+                            <FooterLink to="/">Agency</FooterLink>
+                            <FooterLink to="/">Influencers</FooterLink>
+                        </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>Social Media</FooterLinkTitle>
+                            <FooterLink to="/">Instagram</FooterLink>
+                            <FooterLink to="/">Facebook</FooterLink>
+                            <FooterLink to="/">YouTube</FooterLink>
+                            <FooterLink to="/">Twitter</FooterLink>
+                            <FooterLink to="/">LinkedIn</FooterLink>
+                        </FooterLinkItems>
+                    </FooterLinksWrapper>
+                </FooterLinksContainer>
+                <SocialMedia>
+                    <SocialMediaWrap>
+                        <SocialLogo to="/" onClick={toggleHome}>Piggy Banking</SocialLogo>
+                        <WebsiteRights>dolla © {new Date().getFullYear()} All rights reserved.</WebsiteRights>
+                        <SocialIcons>
+                            <SocialIconLink href="/" target="_blank" aria-label="Facebook">
+                                <FaFacebook />
+                            </SocialIconLink>
+                            <SocialIconLink href="/" target="_blank" aria-label="Instagram">
+                                <FaInstagram />
+                            </SocialIconLink>
+                            <SocialIconLink href="/" target="_blank" aria-label="YouTube">
+                                <FaYoutube />
+                            </SocialIconLink>
+                            <SocialIconLink href="/" target="_blank" aria-label="Twitter">
+                                <FaTwitter />
+                            </SocialIconLink>
+                            <SocialIconLink href="//www.linkedin.com/in/nkwon88" target="_blank" aria-label="LinkedIn">
+                                <FaLinkedin />
+                            </SocialIconLink>
+                        </SocialIcons>
+                    </SocialMediaWrap>
+                </SocialMedia>
+            </FooterWrap>
+        </FooterContainer>
+    )
+}
+
+export default Footer
---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,50 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Premium Banking',
+    headline: 'Unlimited Transactions with Zero Fees',
+    description: 'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees',
+    buttonLabel: 'Get Started',
+    imgStart: false,
+    img: require('../../images/svg-1.svg').default,
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+};
+
+export const homeObjTwo = {
+    id: 'discover',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Unlimited Access',
+    headline: 'Login to our account at any time',
+    description: 'We have you covered no matter where you are located. All you need is an internet onnection and a phone or computer.',
+    buttonLabel: 'Learn More',
+    imgStart: true,
+    img: require('../../images/svg-2.svg').default,
+    alt: 'minus-percent',
+    dark: false,
+    primary: false,
+    darkText: true
+};
+
+export const homeObjThree = {
+    id: 'signup',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Join Our Team',
+    headline: 'Creating an account is extremely easy',
+    description: "Get everything set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go.",
+    buttonLabel: 'Start Now',
+    imgStart: false,
+    img: require('../../images/premium-benefits.svg').default,
+    alt: 'Papers',
+    dark: false,
+    primary: false,
+    darkText: true
+};
